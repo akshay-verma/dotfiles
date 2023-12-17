82fd9415661d87654faab08709358add503d5f25
Akshay Verma <15175239+akshay-verma@users.noreply.github.com>
2023-12-18 00:21:43 +0530

    lazy lua config

---
+++ nvim/lua/plugins/disabled.lua
@@ -0,0 +1,5 @@
+return {
+  -- disable neo-tree
+  { "echasnovski/mini.pairs", enabled = false },
+  { "nvim-neo-tree/neo-tree.nvim", enabled = false },
+}
--- nvim/lua/plugins/nvim-treesitter.lua
+++ nvim/lua/plugins/nvim-treesitter.lua
@@ -2,5 +2,16 @@ return {
   "nvim-treesitter/nvim-treesitter",
   opts = function(_, opts)
     opts.ignore_install = { "help" }
+
+    -- To extend the default config, use the code below:
+    vim.list_extend(opts.ensure_installed, {
+      "lua",
+      "vim",
+      "python",
+      "tsx",
+      "json",
+      "bash",
+      "typescript",
+    })
   end,
 }
